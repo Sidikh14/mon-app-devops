ca323fbb6f8bd6182f338eaf9351e90a6ef8b4af
Sidikh14 <sidikhsene43@gmail.com>
2026-08-13 11:52:34 +0000

    feat: premier commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.send(`
+        <div style="text-align: center; font-family: sans-serif; padding-top: 50px;">
+            <h1 style="color: #2b6cb0;">🚀 Mon Application DevOps est en Ligne !</h1>
+            <p style="font-size: 1.2rem; color: #4a5568;">Cette application tourne dans un conteneur Docker.</p>
+            <span style="background: #e2e8f0; padding: 10px 20px; border-radius: 5px; font-weight: bold;">
+                Status: OK 🟢
+            </span>
+        </div>
+    `);
+});
+
+app.listen(PORT, () => {
+    console.log(`Serveur démarré sur http://localhost:${PORT}`);
+});
