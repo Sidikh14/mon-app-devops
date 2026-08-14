e622394fdb72443140e2f997378ae4a1f44d64f0
Sidikh14 <sidikhsene43@gmail.com>
2026-08-14 08:41:56 +0000

    suppression modification

--- server.js
+++ server.js
@@ -5,9 +5,8 @@ const PORT = 3000;
 app.get('/', (req, res) => {
     res.send(`
         <div style="text-align: center; font-family: sans-serif; padding-top: 50px;">
-            <h1 style="color: #2b6cb0;">🚀 SIDIKH SENE TESTE DEVOPS !</h1>
+            <h1 style="color: #2b6cb0;">🚀 SIDIKH SENE TEST DEVOPS !</h1>
             <p style="font-size: 1.2rem; color: #4a5568;">Cette application tourne dans un conteneur Docker que je nomme SAGFA.</p>
-           <p style="font-size: 1.2rem; color:#4a5462;">Seydicifer est GAY</P> 
             <span style="background: #e2e8f0; padding: 10px 20px; border-radius: 5px; font-weight: bold;">
                 Status: OK 🟢
             </span>
